077d15b3b15e35e5d04e8b435f9fbf38b8c998c7
MUHAMMED YASEEN N A <121yaseen@gmail.com>
2021-09-26 10:29:05 +0530

    getRepos

--- command.js
+++ command.js
@@ -1,5 +1,5 @@
 const program = require("commander");
-const { sayHello } = require("./index");
+const { sayHello, getRepos } = require("./index");
 
 program.version("1.0.0").description("Bob hack");
 
@@ -9,6 +9,13 @@ program
   .description("Say Hello")
   .action(() => {
     sayHello();
+  });
+
+program
+  .command("getrepos <currpage> <pagesize>")
+  .alias("gr")
+  .action((currpage, pagesize) => {
+    getRepos(currpage, pagesize);
   });
 
 program.parse(process.argv);
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+const { default: axios } = require("axios");
+
 const sayHello = () => {
   console.log("Hello World");
 };
 
+const getRepos = (currPage, pageSize) => {
+  axios
+    .get("https://dev-api.metabob.com/repositories/", {
+      params: {
+        current_page: currPage,
+        page_size: pageSize,
+      },
+    })
+    .then((res) => {
+      console.log(res);
+    });
+};
+
 module.exports = {
   sayHello,
+  getRepos,
 };
